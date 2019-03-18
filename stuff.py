5297ad596d42b3922d9200332d501fe69cbf3e29
Jake Coxon <jake.coxon@gmail.com>
2019-03-18 14:57:23 +0000

    Elements

--- stuff.py
+++ stuff.py
@@ -6,7 +6,13 @@ class Visitor:
         self.root = root
         self.parents = [root]
         self.ids = [-1]
-        self.current_node = None
+        # self.current_node = None
+
+    @property
+    def current_node(self):
+        if self.current_id >= len(self.current_parent.children):
+            return None
+        return self.current_parent.children[self.current_id]
 
     @property
     def current_parent(self):
@@ -18,28 +24,28 @@ class Visitor:
 
     def get_next_node(self):
         next_id = self.current_id + 1
-        if next_id >= len(self.current_parent[1]):
+        if next_id >= len(self.current_parent.children):
             return None
-        node = self.current_parent[1][next_id]
-        self.ids[-1] += 1
+        node = self.current_parent.children[next_id]
         return node
 
     def rest_children(self):
         id = self.current_id
-        while id < len(self.current_parent[1]):
-            yield self.current_parent[1][id]
+        while id < len(self.current_parent.children):
+            yield self.current_parent.children[id]
             id += 1
 
     def next_node(self):
-        self.current_node = self.get_next_node()
+        current_node = self.get_next_node()
+        self.ids[-1] += 1
 
     def enter_node(self):
         self.parents.append(self.current_node)
-        self.current_node = None
+        # self.current_node = None
         self.ids.append(-1)
     
     def exit_node(self):
-        self.current_node = self.current_parent
+        # self.current_node = self.current_parent
         self.parents.pop()
         self.ids.pop()
 
@@ -47,30 +53,30 @@ class Visitor:
 def get_matching_node(visitor, match_node, node_type, key):
     if match_node is None:
         return None
-    if match_node[0] == node_type:
+    if Element.compare_types(match_node == node_type):
         return match_node
 
     for match_node in visitor.rest_children():
-        if match_node[0] == node_type:
+        if Element.compare_types(match_node, node_type):
             return match_node
 
 
 def create_node(visitor, node_type, key=None):
     print(f"Creating node {node_type} with key {key}")
-    return [node_type, []]
+    return Element(node_type)
 
 
 def align_with_node(visitor, node_type, key=None):
     existing_node = get_matching_node(visitor, visitor.current_node, node_type, key)
     if existing_node is not None:
-        print(f"Removing {existing_node[0]}")
-        visitor.current_parent[1].remove(existing_node)
+        print(f"Removing {existing_node}")
+        visitor.current_parent.remove_remove(existing_node)
     node = existing_node or create_node(visitor, node_type, key)
     # TODO: Some stuff here
     if node is None:
         return
-    print(f"Inserting {node[0]}")
-    visitor.current_parent[1].insert(visitor.current_id, node)
+    print(f"Inserting {node}")
+    visitor.current_parent.add_child(visitor.current_id, node)
 
 
 def clear_unvisited(visitor, parent, node):
@@ -105,8 +111,9 @@ def node_visitor(visitor):
 
 
 class Element:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, widget, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.widget = widget
         self.children = []
         
     def remove_child(self, child):
@@ -116,40 +123,56 @@ class Element:
         self.children.insert(index, child)
 
     @classmethod
-    def compare_elements(cls, first, second):
-        if first.__class__ != second.__class__:
+    def compare_types(cls, element, new_widget):
+        if element.widget.__class__ != new_widget.__class__:
             return False
+        return True
+
+
+
+
+
+
+class Background:
+    def __init__(self, color):
+        self.color = color
+
+
+class Padding:
+    def __init__(self, inset):
+        self.inset = inset
+
+
+class Text:
+    def __init__(self, label):
+        self.label = label
 
 
 if __name__ == "__main__":
 
-    root = ["root", [
-        [1, [
-            [2, [
-                [4, []],
-                [4, []],
-            ]],
-            [8, [
-                [1, []]
-            ]]
-        ]]
-    ]]
+    root = Element(None)
     visitor = Visitor(root)
     container = node_visitor(visitor)
+
+    def container(node, *args):
+        return node_visitor(visitor)(node)
 
     def node(node_type):
         open_node(visitor, node_type)
         close_node(visitor)
 
-    with container(1):
-        with container(2):
-            node(4)
-            node(2)
+    with container(
+        Background('red'),
+        # Padding(20)
+    ): 
+        node(Text("Hello"))
+        node(Text("What is this"))
 
-            for i in range(3):
-                node(7)
+        for i in range(3):
+            node(Text(f"Label {i}"))
 
-    print(json.dumps(root, indent=2))
+    print(root)
+    # print(json.dumps(root, indent=2))
 
 
 # def something():
