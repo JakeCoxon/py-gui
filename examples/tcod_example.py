c4d424a3c8304b34a3fe9ba009534509b93f003b
Jake Coxon <jake.coxon@gmail.com>
2019-03-30 23:29:25 +0000

    Updates

--- examples/tcod_example.py
+++ examples/tcod_example.py
@@ -2,28 +2,55 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
 import tcod
-from gui.core import Container, Node, Padding, Size, ColumnLayout
-from gui.tcod import BackgroundColor, Text, start_app
+from gui.core import Container, Node, Padding, Size, ColumnLayout, Align, StackLayout
+from gui.tcod import BackgroundColor, Text, Border, Darken, start_app
 from random import random as r
 
+
 def gui_func(ctx):
-    with Container(
-        Padding.all(1),
-        ColumnLayout(spacing=1)
-    ):
-        Node(
-            BackgroundColor(200, 0, 0),
-            Size.height(4)
-        )
-        Node(
-            BackgroundColor(40, 100, 40),
+    with Container(StackLayout()):
+        with Container(
+            BackgroundColor(3, 71, 50),
             Padding.all(1),
-            Text("Hello")
-        )
+            ColumnLayout(spacing=1)
+        ):
+            Node(
+                BackgroundColor(0, 129, 72),
+                Padding.all(1),
+                Align.right,
+                Text("Hello")
+            )
+            Node(
+                BackgroundColor(0, 129, 72),
+                Padding.all(1),
+                Align.centerX,
+                Text("Hello")
+            )
+            Node(
+                BackgroundColor(221, 215, 22),
+                Padding.all(4),
+                Text("Some text", color=(0, 129, 72))
+            )
+            with Container(
+                BackgroundColor(239, 138, 23),
+                Border(),
+                ColumnLayout(),
+            ):
+                Node(Border(), Text("Some text"))
+                Node(
+                    BackgroundColor(239, 41, 23),
+                    Border(color=(87, 15, 9)),
+                    Padding.all(1),
+                    Text("Some text", color=(87, 15, 9))
+                )
+                Node(Text("Some text"))
+        
         Node(
-            BackgroundColor(40, 40, 200),
-            Padding.all(4),
-            Text("Some text")
+            Padding.all(5),
+            Darken(0.8),
+            Border(),
+            Padding.all(5),
+            Text("Hello")
         )
 
 
